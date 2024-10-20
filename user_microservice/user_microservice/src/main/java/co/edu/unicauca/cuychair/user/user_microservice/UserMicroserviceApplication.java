821121ed8f30a12f880dfd95e116f09616c409a6
Juliandross <111920433+juliandross@users.noreply.github.com>
2024-10-20 18:33:14 -0500

    GUI intentando lookandfeel

--- user_microservice/user_microservice/src/main/java/co/edu/unicauca/cuychair/user/user_microservice/UserMicroserviceApplication.java
+++ user_microservice/user_microservice/src/main/java/co/edu/unicauca/cuychair/user/user_microservice/UserMicroserviceApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserMicroserviceApplication {
+public class UserMicroserviceApplication{
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		SpringApplication.run(UserMicroserviceApplication.class, args);
 	}
-
 }
