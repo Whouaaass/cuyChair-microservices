dc6f7ce1ea5df75751178f23c48cfd0eaee89b57
Juliandross <111920433+juliandross@users.noreply.github.com>
2024-10-20 13:30:03 -0500

    gui

---
+++ gui/gui/src/main/java/co/edu/unicauca/cuychair/gui/gui/Gui.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.unicauca.cuychair.gui.gui;
+
+/**
+ *
+ * @author julia
+ */
+public class Gui {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
