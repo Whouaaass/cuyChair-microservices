6efa346ea2fccec9a467a4118510bf76373be044
Juliandross <111920433+juliandross@users.noreply.github.com>
2024-10-26 18:03:37 -0500

    arreglos dto

--- gui/gui/src/main/java/co/edu/unicauca/cuychair/gui/gui/controller/userController.java
+++ gui/gui/src/main/java/co/edu/unicauca/cuychair/gui/gui/controller/userController.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package co.edu.unicauca.cuychair.gui.gui.services;
+package co.edu.unicauca.cuychair.gui.gui.controller;
 
 /**
  *
  * @author julia
  */
-public class conferenceServices {
+public class userController {
     
 }
