bb28aa0404c4cec334b077f7462ef5adf465b718
Whouaaass <fredyanaya@unicauca.edu.co>
2024-12-08 20:34:13 -0500

    Starting to fix

--- api_gateway/src/main/java/co/edu/unicauca/cuychair/api_gateway/Controller/GatewayController.java
+++ api_gateway/src/main/java/co/edu/unicauca/cuychair/api_gateway/Controller/GatewayController.java
@@ -1,23 +1,15 @@
 package co.edu.unicauca.cuychair.api_gateway.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.WebSession;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import reactor.core.publisher.Mono;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.WebSession;
 
 @RestController
 public class GatewayController{
-    
-    @GetMapping("/")
-    public Mono<String> index(WebSession session){
-        return Mono.just(session.getId());
-    }
-
-    @GetMapping("/token")
-    public Mono<String> getToken(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient client){
-        return Mono.just(client.getAccessToken().getTokenValue());
+    @GetMapping("/api")
+    public String api(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient, WebSession session){
+        return "API Gateway";
     }
 }
