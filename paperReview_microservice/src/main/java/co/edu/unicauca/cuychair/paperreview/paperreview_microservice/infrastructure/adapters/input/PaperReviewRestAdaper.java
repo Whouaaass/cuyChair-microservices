0ee4f203e102d950de80903f1e51b9d482d68411
Whouaaass <fredyanaya@unicauca.edu.co>
2024-12-08 20:59:36 -0500

    todo hex

--- paperReview_microservice/src/main/java/co/edu/unicauca/cuychair/paperreview/paperreview_microservice/infrastructure/adapters/input/PaperReviewRestAdaper.java
+++ paperReview_microservice/src/main/java/co/edu/unicauca/cuychair/paperreview/paperreview_microservice/infrastructure/adapters/input/PaperReviewRestAdaper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,13 +18,17 @@ import co.edu.unicauca.cuychair.paperreview.paperreview_microservice.domain.enti
 @RestController
 @RequestMapping("/paperReview")
 public class PaperReviewRestAdaper {
+
+
     private final ServicePaperReviewPort service;
     
     public PaperReviewRestAdaper(ServicePaperReviewPort service) {
         this.service = service;
     }
-    @GetMapping("/changeState/{paperReviewId}/{newState}") // Ruta completa: /paperReview/changeState/{paperReviewId}/{newState}
-    public boolean changeState(@PathVariable int paperReviewId, @PathVariable String newState) {
+    
+    @PatchMapping("/changeState/{paperReviewId}/{newState}") // Ruta completa: /paperReview/changeState/{paperReviewId}/{newState}
+    public boolean changeState(@PathVariable int paperReviewId, @PathVariable String newState, @RequestBody String comment) {
+
         return service.changeState(paperReviewId, newState);
     }
 
@@ -37,6 +42,11 @@ public class PaperReviewRestAdaper {
         return service.getPaperReview(paperReviewId);
     }
 
+    @GetMapping("/getByPaper/{paperId}") // Ruta completa: /paperReview/getByPaper/{paperId}
+    public paperReview getPaperReviewByPaper(@PathVariable int paperId) {
+        return null; //service.getPaperReviewByPaper(paperId);
+    }
+
     @DeleteMapping("/delete/{paperReviewId}") // Ruta completa: /paperReview/delete/{paperReviewId}
     public paperReview deletePaperReview(@PathVariable int paperReviewId) {
         return service.deletePaperReview(paperReviewId);
@@ -46,5 +56,9 @@ public class PaperReviewRestAdaper {
     public List<paperReview> listPaperReviews() {
         return service.listPaperReview();
     }
-    
+
+    @GetMapping("/listByConference/{conferenceId}") // Ruta completa: /paperReview/listByReviewer/{reviewerId}
+    public List<paperReview> listPaperReviewsByReviewer(@PathVariable int conferenceId) {
+        return null; // service.listPaperReviewByConference(conferenceId);
+    }    
 }
