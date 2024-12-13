f955b863d3e5dfff173a0899861e2878b8fe12a4
Juliandross <111920433+juliandross@users.noreply.github.com>
2024-12-13 06:35:28 -0500

    cinco

--- paperReview_microservice/src/main/java/co/edu/unicauca/cuychair/paperreview/paperreview_microservice/infrastructure/adapters/input/PaperReviewRestAdaper.java
+++ paperReview_microservice/src/main/java/co/edu/unicauca/cuychair/paperreview/paperreview_microservice/infrastructure/adapters/input/PaperReviewRestAdaper.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +28,7 @@ public class PaperReviewRestAdaper {
         this.service = service;
     }
     //Importante, funciona con el id del paperReview, no del paper
-    @PatchMapping("/changeState/{paperReviewId}/{reviewerId}/{newState}") // Ruta completa: /paperReview/changeState/{paperReviewId}/{newState}
+    @PutMapping("/changeState/{paperReviewId}/{reviewerId}/{newState}") // Ruta completa: /paperReview/changeState/{paperReviewId}/{newState}
     public Result changeState(@PathVariable int paperReviewId,@PathVariable int reviewerId,@PathVariable String newState) {
         return service.changeState(paperReviewId, reviewerId,newState);
     }
